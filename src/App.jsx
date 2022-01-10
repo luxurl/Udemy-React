2986954115d693d6bede94ed3c96e05fd1bc807c
luxurl <78976230+luxurl@users.noreply.github.com>
2022-01-10 18:39:27 +0900

    絵文字のON/OFFボタンの実装

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ import React, { useState } from "react";
 import ColofulMessage from "./compornents/ColorFulMessage";
 
 const App = () => {
+  //ステートの変数名とセッターを分割代入
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   //ボタンを押すとカウントアップする処理
   const onClicCountUp = () => {
     setNum(num + 1);
   };
-  //ステートの変数名とセッターを分割代入
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
   return (
     //エラーを回避するためだけの囲い
     //    <React.Fragment>
@@ -20,7 +27,9 @@ const App = () => {
       <ColofulMessage color="blue">How are you?</ColofulMessage>
       <ColofulMessage color="pink">I'm fine</ColofulMessage>
       <button onClick={onClicCountUp}>botton</button>
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(*^^*)</p>}
     </>
     //   </React.Fragment>
   );
