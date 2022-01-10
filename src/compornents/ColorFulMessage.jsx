af46e884a20481d971be8c3f06afc8fd8ea5dc7d
luxurl <78976230+luxurl@users.noreply.github.com>
2022-01-10 18:20:42 +0900

    props応用

--- src/compornents/ColorFulMessage.jsx
+++ src/compornents/ColorFulMessage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  const { color, children } = props;
   console.log(props);
 
   const contentStyle = {
-    color: props.color,
+    //プロパティと同じ変数名の場合省略可
+    color,
     fontSize: "18px"
   };
 
-  return <p style={contentStyle}>{props.children}</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
 
 export default ColorfulMessage;
